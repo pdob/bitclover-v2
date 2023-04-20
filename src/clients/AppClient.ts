1bc62ec8a9bbbf42cb75bcdd8b3a4874ab72b922
pdob <patdob95@gmail.com>
2023-04-20 21:52:49 +0200

    Add methods to client to fetch individual coin data and coin chart data

--- src/clients/AppClient.ts
+++ src/clients/AppClient.ts
@@ -27,6 +27,18 @@ class AppClient {
     const response = await this.makeRequest<CoinData[]>(`markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
     return response
   }
+
+  public async getCoinInfo(id: string) {
+    const response = await this.makeRequest(`${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false`)
+    return response
+  }
+
+  public async getCoinChartData(id: string, currency: SupportedCurrencies, days: number) {
+    const response = await this.makeRequest(`${id}/market_chart?vs_currency=${currency}&days=${days}&interval=${
+      days === 1 || days === 7 || days == 30 ? 'hourly' : 'daily'
+    }`)
+    return response
+  }
 }
 
 const appClient = new AppClient()
