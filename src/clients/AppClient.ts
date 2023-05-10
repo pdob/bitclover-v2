0841cf384a397f665d736a636b8d30919f41de79
pdob <patdob95@gmail.com>
2023-05-10 12:26:10 +0100

    Add exchange endpoint to AppClient

--- src/clients/AppClient.ts
+++ src/clients/AppClient.ts
@@ -3,11 +3,12 @@ import {Config} from 'react-native-config'
 import { TimePeriod } from '../screens/CoinInfo'
 import { ChartData } from '../types/CoinInfo'
 import { CoinData, SupportedCurrencies } from '../types/Home'
+import { ExchangeItem } from '../screens/Exchanges'
 
 class AppClient {
   private API_URL: string
 
-  constructor(apiUrl: string = Config.API_URL || 'https://api.coingecko.com/api/v3/coins/') {
+  constructor(apiUrl: string = Config.API_URL || 'https://api.coingecko.com/api/v3/') {
     this.API_URL = apiUrl
   }
   
@@ -21,27 +22,31 @@ class AppClient {
     return `ids=${formattedIds}`
   }
   
-  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
-  public async getAllCoinPrices(currency: SupportedCurrencies, perPage: number = 100) {
-    const response = await this.makeRequest<CoinData[]>(`markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=1&sparkline=false&locale=en`)
+  public async getAllCoinPrices(currency: SupportedCurrencies, perPage = 100) {
+    const response = await this.makeRequest<CoinData[]>(`coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=1&sparkline=false&locale=en`)
     return response
   }
 
   public async getCoinInfo(id: string): Promise<CoinData> {
-    const response = await this.makeRequest(`${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false`)
+    const response = await this.makeRequest(`coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false`)
     return response.market_data
   }
 
   public async getCoinChartData(id: string, currency: SupportedCurrencies, days: TimePeriod): ChartData {
-    const response = await this.makeRequest(`${id}/market_chart?vs_currency=${currency}&days=${days}&interval=${
+    const response = await this.makeRequest(`coins/${id}/market_chart?vs_currency=${currency}&days=${days}&interval=${
       days <= 30 ? 'hourly' : 'daily'
     }`)
     return response
   }
 
   public async fetchFavourites(ids: string[], currency: SupportedCurrencies): Promise<CoinData[] | []>{
-    const response: CoinData[] = await this.makeRequest(`markets?vs_currency=${currency}&${this.formatFavouritesQuery(ids)}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
+    const response = await this.makeRequest<CoinData[]>(`coins/markets?vs_currency=${currency}&${this.formatFavouritesQuery(ids)}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
     return ids.length ? response : []
+  }
+  
+  public async getAllExchanges(perPage = 50): Promise<ExchangeItem[]>{
+    const response = await this.makeRequest<ExchangeItem[]>(`exchanges?per_page=${perPage}`)
+    return response
   }
 }
 
