0c29ac98ab3aabc02772711224852557e5c50eac
pdob <patdob95@gmail.com>
2023-06-27 18:03:06 +0100

    Add tests for Settings

---
+++ src/__tests__/Settings.test.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { fireEvent, render, cleanup } from '@testing-library/react-native'
+import Settings from '../screens/Settings'
+import configureStore from 'redux-mock-store'
+import { RootState } from '../store/store'
+import { Provider } from 'react-redux'
+import MockNav from '../__mocks__/MockNav'
+
+const mockStore = configureStore([])
+
+const mockState: Partial<RootState> = {
+  settings: {
+    currency: 'USD',
+    initialScreen: 'Home'
+  },
+  favourites: {
+    ids: []
+  }
+}
+
+const mockNav = {
+  navigation: {
+    navigate: jest.fn(),
+  },
+  route: {
+    params: {},
+  },
+}
+
+const mockedStore = mockStore(mockState)
+
+describe('settings screen', () => {
+  afterEach(() => cleanup())
+  const navSpy = jest.spyOn(mockNav.navigation, 'navigate')
+
+  it('renders', () => {
+    const settings = render(
+      <Provider store={mockedStore}>
+        <MockNav component={Settings} />
+      </Provider>
+    )
+    expect(settings).toBeDefined()
+  })
+
+  it('navigates to terms', () => {
+    const { getByTestId } = render(
+      <Provider store={mockedStore}>
+        <Settings 
+          {...mockNav}
+        />
+      </Provider>
+    )
+    const termsButton = getByTestId('Settings_terms')
+    fireEvent.press(termsButton)
+    expect(navSpy).toBeCalledWith('Terms')
+  })
+
+  it('navigates to terms', () => {
+    const { getByTestId } = render(
+      <Provider store={mockedStore}>
+        <Settings 
+          {...mockNav}
+        />
+      </Provider>
+    )
+    const termsButton = getByTestId('Settings_privacy')
+    fireEvent.press(termsButton)
+    expect(navSpy).toBeCalledWith('Privacy')
+  })
+
+  it('shows dropdown and correct options', () => {
+    const { getByText, getByTestId } = render(
+      <Provider store={mockedStore}>
+        <Settings 
+          {...mockNav}
+        />
+      </Provider>
+    )
+
+    expect(getByTestId('Settings_dropdownCurrency')).toBeDefined()
+    expect(getByText('USD')).toBeDefined()
+    expect(getByText('AUD')).toBeUndefined()
+  })
+})
